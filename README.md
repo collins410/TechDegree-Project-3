914a79baf07b8c7c55fce59e0bd12c280fc41749
John Collins <j.k.collins410@gmail.com>
2019-08-06 20:30:17 -0400

    Squashed a bug with the checkboxes not disabling and enabling as expected     Corrected an error with the form submission     Correctly hiding and showing the bitcoin and paypal paragraphs now

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 JavaScript is used to enhance an interactive form.
 
-The JavaScript behind the interactivity of the form was created using jQuery.
+The JavaScript behind the interactivity of the form was created using jQuery.  Every attempt was made to only use jQuery.
     
     -User input is validated
     -Messages are displayed for incorrect input
@@ -11,3 +11,40 @@ The JavaScript behind the interactivity of the form was created using jQuery.
     -The Email field must be of a valid email format (existance of the email account is not validated)
     -At least 1 checkbox must be selected in the Activites section
     -Only one form of payment is accepted
+
+Update 1.0
+    Updated index.html with minor formatting changes
+    
+    Created script.js to inlcude the following functionality:
+    
+    //Place the first field in the focus state on page load
+    
+    //As a user enters their email address the email address is dynamically checked for validity
+    
+    //Hide the "Job Role" text box with JavaScript initially, then show it with JS upon selection of Job Role when a valid
+    //role is de-selected
+    
+    // When a theme is selected color options are shown and displays "Please select a T-shirt theme"
+    // Only the color options that match the theme selected are displayed
+    // When a new theme is selected the color field and drop down menu update to match the selected theme
+    
+    //Detects a conflict in any checkboxes data-time-and-day field using regular expressions
+    //Dynamically reads the data-day-and-time attribute and retrieves the correct value, whether day or time
+    //Append a tag to any time/day conflicts and disable the corresponding checkbox
+    
+    //The total value is updated based on the class selections and corresponding costs
+    
+    //The payment menu is set to credit card by default
+    
+    //The credit card fields are hidden when credit card is de-selected
+    
+    //The credit card number is validated prior to form submission and the user is alerted of an invalid credit card number dynamically as it is entered
+    //An invalid credit card entry displays one message
+    //A credit card number of a different length displays another message
+    
+    Minor changes were made to style.css - in the future more changes will be made to the form appearance
+
+Update 1.1
+    Squashed a bug with the checkboxes not disabling and enabling as expected
+    Corrected an error with the form submission 
+    Correctly hiding and showing the bitcoin and paypal paragraphs now
--- js/script.js
+++ js/script.js
@@ -6,14 +6,9 @@ const tomato = $colorArray[0][3];
 const steelBlue = $colorArray[0][4];
 const dimGrey = $colorArray[0][5];
 const unavailable = `<div id="unavailable">This item has a scheduling conflict and is not available.</div>`;
-let $checked = $('input:checked');
-let $notChecked = $('input[type=checkbox]').not(':checked');
 let total = 0;
 let formHtml = '<div class="message"> Please enter a valid credit card number</div>';
-//removes the "Main Conference from $notChecked due to it not having the data-day-and-time attribute
-$notChecked = $.grep($notChecked, function(e){
-  return e !== $('[name=all]')[0];
-});
+
 
 //Places the first field in the focus state on page load
 $('#name').focus();
@@ -22,13 +17,13 @@ $('#name').focus();
 //role is de-selected
 $('#other-title').hide('#other-title');
 $('#title').change(function (){
-      //let $str = $(this).find("option:selected").text();
-      if($('#title').val()=== 'other'){
-            $('#other-title').fadeIn();
-      }
-      if ($('#title').val() !== 'other'){
-            $('#other-title').fadeOut('#other-title');
-      }
+  //let $str = $(this).find("option:selected").text();
+  if($('#title').val()=== 'other'){
+    $('#other-title').fadeIn();
+  }
+  if ($('#title').val() !== 'other'){
+    $('#other-title').fadeOut('#other-title');
+  }
 });
 
 //Hides the Color Menu until a design is selected
@@ -38,26 +33,26 @@ $('#colors-js-puns').hide('colors-js-puns');
 // Only displays the color options that match the theme selected
 // When a new theme is selected the color field and drop down menu update
 $('#design').change(function() {
-      let $str = $(this).find("option:selected").text();
-      if(($str.indexOf("Puns")) > 0) {
-            $('#color').empty()
-                .append(cornFlowerBlue)
-                .append(darkSlateGrey)
-                .append(gold)
-                .append('<option value="Please_select" selected="selected">Please select a T-shirt theme...</option>');
-            $('#colors-js-puns').fadeIn('colors-js-puns');
-      }
-      else if(($str.indexOf("I")) > 0){
-            $('#color').empty()
-                .append(tomato)
-                .append(steelBlue)
-                .append(dimGrey)
-                .append('<option value="Please_select" selected="selected">Please select a T-shirt theme...</option>');
-            $('#colors-js-puns').fadeIn('colors-js-puns');
-      }
-      if(($str.indexOf("Select")) >= 0) {
-            $('#colors-js-puns').fadeOut('colors-js-puns');
-      }
+  let $str = $(this).find("option:selected").text();
+  if(($str.indexOf("Puns")) > 0) {
+    $('#color').empty()
+        .append(cornFlowerBlue)
+        .append(darkSlateGrey)
+        .append(gold)
+        .append('<option value="Please_select" selected="selected">Please select a T-shirt theme...</option>');
+    $('#colors-js-puns').fadeIn('colors-js-puns');
+  }
+  else if(($str.indexOf("I")) > 0){
+    $('#color').empty()
+        .append(tomato)
+        .append(steelBlue)
+        .append(dimGrey)
+        .append('<option value="Please_select" selected="selected">Please select a T-shirt theme...</option>');
+    $('#colors-js-puns').fadeIn('colors-js-puns');
+  }
+  if(($str.indexOf("Select")) >= 0) {
+    $('#colors-js-puns').fadeOut('colors-js-puns');
+  }
 });
 
 
@@ -78,15 +73,18 @@ $('#mail').keyup(function (e){
 
 //Detects a conflict in check boxes
 $('input:checkbox').click(function (event){
+  let $notChecked = $('input[type=checkbox]').not(':checked');
   //compare the clicked checkbox to the unclicked checkboxes and disable all matching dates/times
   let thisDay = returnDay($(this));
   let thisTime = returnTime($(this));
   let $disabledCheckbox = [];
-  $checked = $('input:checked');
-
   //removes the target from the $notChecked array
   $notChecked = $.grep($notChecked, function(e){
     return e !== event.target;
+  });
+  //removes the "Main Conference from $notChecked due to it not having the data-day-and-time attribute
+  $notChecked = $.grep($notChecked, function(e){
+    return e !== $('[name=all]')[0];
   });
 
   //updates the value of total based on the class selections
@@ -97,19 +95,21 @@ $('input:checkbox').click(function (event){
   }
 
   // Searches the unchecked checkboxes for conflicting times and appends a tag to any time/day conflicts
-  if ($(event.target)[0] !== $('[name=all]')[0]){
-    $.each($notChecked, function () {
-      if ($(this).attr('data-day-and-time')) {
-        let day = returnDay($(this));
-        let time = returnTime($(this));
-        if ((thisDay[0] === day[0]) && (thisTime[0] === time[0])) {
-         $(this).attr('disabled', true);
-          $(this).closest('label').append(unavailable);
-          $disabledCheckbox.push($(this));
-        }
+
+  $.each($notChecked, function () {
+    if ($(this).attr('data-day-and-time')) {
+      let day = returnDay($(this));
+      let time = returnTime($(this));
+      if ((thisDay === null)) {
+        return
+      }else if ((thisDay[0] === day[0]) && (thisTime[0] === time[0])) {
+        $(this).attr('disabled', true);
+        $(this).closest('label').append(unavailable);
+        $disabledCheckbox.push($(this));
       }
-    });
-  }
+    }
+  });
+
   //select the disable link which corresponds to the conflict and when the conflict is resolved (box unchecked)
   //remove the corresponding div and enable the correct disabled checkbox
   if(!$(event.target).is(':checked')){
@@ -144,28 +144,35 @@ function returnTime (object){
 //Appends the total cost of all classes and updates based on checked activities
 function updateTotalField (){
   let $totalField =`<div id="total" class="total-cost">The total cost is:  </div>`;
-      $('#total').remove();
-      $('#total').remove();
-      $('#payment').parent().prepend($totalField);
-      $('#payment').parent().append($totalField);
+  $('#total').remove();
+  $('#total').remove();
+  $('#payment').parent().prepend($totalField);
+  $('#payment').parent().append($totalField);
   $('.total-cost').html(`The total cost is:  $${total}`);
 }
 
-// Set the payment menu to the credit card by default
+// Set the payment menu to the credit card by default, show the credit card field and hide the bitcoin and paypal paragraphs
 $('#payment').val('credit card');
-
+$('#p-bitcoin').hide();
+$('#p-paypal').hide();
 //when payment options are selected the display updates accordingly
 $('#payment').change(function(){
   let value = this.value;
   let $option = $('option:selected', this);
   if (value === 'paypal'){
+    $('#p-paypal').show();
     $('#credit-card').hide();
+    $('#p-bitcoin').hide();
   }
   if (value === 'bitcoin'){
+    $('#p-bitcoin').show();
     $('#credit-card').hide();
+    $('#p-paypal').hide();
   }
   if (value === 'credit card'){
     $('#credit-card').show();
+    $('#p-bitcoin').hide();
+    $('#p-paypal').hide();
   }
 });
 
@@ -283,9 +290,9 @@ $('button').click(function(e){
 
   //Validates an email has been entered
   if ((email === null) || (name.length === 0)) {
-   if($('#name').siblings().length === 7) {
-     $('[for=name]').parent().prepend(validationHtml);
-   }
+    if($('#name').siblings().length === 7) {
+      $('[for=name]').parent().prepend(validationHtml);
+    }
   }else if ((email.length > 0) && (($('#name').siblings().length === 7) || ($('#name').siblings().length === 8) ) && (name.length > 0)){
     emailValid = true;
     $('[for=name]').siblings('.message').remove();
@@ -293,8 +300,10 @@ $('button').click(function(e){
 
   // Checks entire form validation and allows form submission if all areas evaluate to validated
   if ((ccValid === true) && (totalValid === true) && (zipValid === true) && (cvvValid === true) && (nameValid === true) && (emailValid === true) ) {
-    alert('submit button was clicked and form validation was completed successfully');
+    alert('form validation was completed successfully');
+    $('form').submit();
   } else if (($('#payment').val() === 'paypal') && (totalValid === true) && (nameValid === true) && (emailValid === true)){
-    alert('submit button was clicked and form validation was completed successfully');
+    alert('form validation was completed successfully');
+    $('form').submit();
   }
 });
